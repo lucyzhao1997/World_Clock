86f5f94275beedcff9ea74fc157a036f9cb432f2
ZIYU ZHAO <Z248ZHAO@edu.uwaterloo.ca>
2019-05-06 21:38:56 -0400

    adding files

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function selector() {
+  var currentDate = new Date();
+  var timeToronto = currentDate.getTime();
+  var timeUTC = timeToronto + 4*60*60*1000;
+  var city = document.getElementById('city');
+  var timeDifference = cityElement.options[city.selectedIndex].value;
+  var timeCity = timeUTC + timeDifference*60*60*1000;
+  currentDate = new Date(timeCity);
+    
+  var hour = currentDate.getHours();
+  var min = currentDate.getMinutes();
+  var sec = currentDate.getSeconds();
+  
+  if (hour < 10) hour = "0" + hour;
+  if (min < 10) min = "0" + min;
+  if (sec < 10 ) sec = "0" + sec;
+  
+  var timeElement = document.getElementById('time');
+  timeElement.innerHTML = hour + ":" + min + ":" + sec;
+  
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth() + 1;
+  var day = currentDate.getDate();
+  
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+  
+  var weekArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satuarday'];
+  var week = weekArray[currentDate.getDay()];
+  var dateElement = document.getElementById('date');
+  dateElement.innerHTML = year + "/" + month + "/"
+ + day + " " + week;
+}
