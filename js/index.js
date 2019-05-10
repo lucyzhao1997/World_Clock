fd29cfc43f47950d9733bc49d13ac9e42f9f518b
ZIYU ZHAO <Z248ZHAO@edu.uwaterloo.ca>
2019-05-09 20:27:30 -0400

    adding comments for js file

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 	function selector() {
 	  var currentDate = new Date();
 	  var timeToronto = currentDate.getTime();
+	  //calculate the UTC time
 	  var timeUTC = timeToronto + 4*60*60*1000;
 	  var city = document.getElementById('city');
 	  var timeDifference = city.options[city.selectedIndex].value;
-	  var cityOrder = city.options[city.selectedIndex].value;
+	  //var cityOrder = city.options[city.selectedIndex].value;
 	  
+	  //calculate the time for each city by the scale factor
 	  var timeCity = timeUTC + timeDifference*60*60*1000;
 	  currentDate = new Date(timeCity);
 		
@@ -14,10 +16,12 @@ $(document).ready(function(){
 	  var min = currentDate.getMinutes();
 	  var sec = currentDate.getSeconds();
 	  
+	  //format the numbers to be properly aligned
 	  if (hour < 10) hour = "0" + hour;
 	  if (min < 10) min = "0" + min;
 	  if (sec < 10 ) sec = "0" + sec;
 	  
+	  //assign the time at the template
 	  var timeElement = document.getElementById('time');
 	  timeElement.innerText = hour + ":" + min + ":" + sec;
 	  
@@ -34,6 +38,7 @@ $(document).ready(function(){
 	  dateElement.innerText = year + "/" + month + "/"
 	 + day + " " + week;
 	}
+	//call the method for regular interval of time
 	selector();
 	setInterval(selector,1000);
 });
